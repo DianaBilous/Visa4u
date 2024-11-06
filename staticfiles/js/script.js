e6e04823dd983854e7d9c6a277fca910418e7c67
Di <db.prokhorova@gmail.com>
2024-11-06 16:09:52 +0100

    Добавлена конфигурация Nginx

---
+++ staticfiles/js/script.js
@@ -0,0 +1,35 @@
+// Подключение к WebSocket
+const chatSocket = new WebSocket(
+    'ws://' + window.location.host + '/ws/chat/'
+);
+
+chatSocket.onmessage = function(e) {
+    const data = JSON.parse(e.data);
+    const chatLog = document.getElementById('chat-log');
+    const newMessage = document.createElement('p');
+    newMessage.innerHTML = `<strong>${data.username}:</strong> ${data.message}`; // Отображение имени пользователя и сообщения
+    chatLog.appendChild(newMessage);
+};
+
+chatSocket.onclose = function(e) {
+    console.error('Чат WebSocket закрыт неожиданно');
+};
+
+document.getElementById('chat-message-submit').onclick = function(e) {
+    const messageInputDom = document.getElementById('chat-message-input');
+    const message = messageInputDom.value;
+    chatSocket.send(JSON.stringify({
+        'message': message
+    }));
+    messageInputDom.value = '';  // Очистка поля ввода
+};
+
+// Функция для открытия чата
+function openChat() {
+    document.getElementById("chat-window").style.display = "block";
+}
+
+// Функция для закрытия чата
+function closeChat() {
+    document.getElementById("chat-window").style.display = "none";
+}
