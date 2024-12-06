55d30f9c3e9426cd3910e83017b2366d44aa0e9f
Di <db.prokhorova@gmail.com>
2024-12-06 08:58:38 +0100

    Изменения

--- apps/visas/apps.py
+++ apps/visas/apps.py
@@ -3,6 +3,3 @@ from django.apps import AppConfig
 class VisasConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'apps.visas'
-
-    def ready(self):
-        import apps.visas.signals
